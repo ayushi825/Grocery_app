f3e6b40536edbbcef1411842db40f7a567a744b8
Ayushi Priya <ayushipriya@example.com>
2024-03-23 17:10:39 +0530

    g

---
+++ backend/sql_connection.py
@@ -0,0 +1,17 @@
+
+import mysql.connector
+
+__cnx = None
+
+def get_sql_connection():
+    global __cnx
+    if __cnx is None:
+        __cnx = mysql.connector.connect(user='root', password='Ayushi25',
+                                        host='127.0.0.1',
+                                        database='grocery_store')
+    return __cnx
+
+
+
+
+
